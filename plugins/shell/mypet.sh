4b79afbf06872f999202fec85cdc1a59a78d02d7
airflow_desktop <ahippo@naver.com>
2024-08-18 14:29:32 +0900

    add shell script example code

---
+++ plugins/shell/mypet.sh
@@ -0,0 +1,9 @@
+
+
+if [ $1 == dog ]; then
+	echo "My cute dog"
+elif [ $1 ==  cat ]; then
+	echo "My cute cat"
+else
+	echo "I have no pet"
+fi
--- plugins/shell/select_fruit.sh
+++
@@ -1,6 +0,0 @@
-FRUIT=$1
-if [ $FRUIT == APPLE ];then
-	echo " You selected Apple"
-else
-	echo " You selected other"
-fi
