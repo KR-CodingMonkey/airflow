89b2dd9702fa0975937ad19ba2be3090bc827bf1
CodingMonkey <ahippo@naver.com>
2024-09-01 22:50:19 +0900

    add python operator example'

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -7,19 +7,19 @@ import random
 
 with DAG(
     dag_id="dags_python_operator",
-    schedule="30 6 * * *",
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    schedule="0 9 * * *",
+    start_date=pendulum.datetime(2024, 8, 25, tz="UTC"),
     catchup=False,
 ) as dag:
-    def select_fruit():
-        fruit = ['APPLE', "BANANA", "ORANGE", "AVOCADO"]
+    def select_pet():
+        pet = ['DOG', "CAT", "MONKEY", "BIRD"]
         rand_int = random.randint(0,3)
-        print(fruit[rand_int])
+        print(pet[rand_int])
 
 
     py_t1 = PythonOperator(
         task_id = "py_t1",
-        python_callable=select_fruit # 어떤 함수를 실행할 건지 명시
+        python_callable=select_pet # 어떤 함수를 실행할 건지 명시
     )
 
     py_t1
