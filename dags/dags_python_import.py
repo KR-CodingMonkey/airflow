d2e8b3ebac1e10dfc6c4ed5625a4a95fdb92dcac
CodingMonkey <ahippo@naver.com>
2024-09-05 23:19:59 +0900

    add python import example

---
+++ dags/dags_python_import.py
@@ -0,0 +1,18 @@
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from common.common import import_python_func
+import datetime
+import pendulum
+
+with DAG(
+    dag_id="dags_python_import",
+    schedule="0 9 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    task_import_func = PythonOperator(
+        task_id = "py_t1",
+        python_callable=import_python_func # 어떤 함수를 실행할 건지 명시
+    )
+
+    task_import_func
