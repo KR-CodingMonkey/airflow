3c8057a8875e2c7512349cf2f7973bb90625a689
laptop <ahippo@naver.com>
2024-09-24 05:21:28 +0900

    add xcom exam2

--- dags/dags_python_xcom_return.py
+++ dags/dags_python_xcom_return.py
@@ -4,7 +4,7 @@ import datetime
 import pendulum
 
 with DAG(
-    dag_id="dags_python_xcom",
+    dag_id="dags_python_xcom_return",
     schedule="0 9 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False,
